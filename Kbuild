20fbc8424d6af55ba0e8b694b107f7c2400ffdaa
Aleksei Makarov <aleksei.makarov@opensynergy.com>
2023-10-11 11:35:35 +0200

    virtio_lo_test.c

--- Kbuild
+++ Kbuild
@@ -1,2 +1,3 @@
-obj-m += virtio-lo.o
+obj-m += virtio-lo.o virtio-lo-test.o
 virtio-lo-y := virtio_lo_device.o virtio_lo_driver.o
+virtio-lo-test-y := virtio_lo_test.o
--- lib/test.h
+++ lib/test.h
@@ -1,12 +1,31 @@
 #ifndef __test_h__
 #define __test_h__
 
-#include <linux/types.h>
+#include <linux/virtio_types.h>
 
 #define VIRTIO_ID_TEST 111
 
+#define VIRTIO_TEST_F_SIZE	0
+#define VIRTIO_TEST_F_MULTIPORT 1
+#define VIRTIO_TEST_F_EMERG_WRITE 2
+
 struct virtio_test_config {
-	__le64 something;
+	__virtio64 something;
+};
+
+#define VIRTIO_TEST_DEVICE_READY	0
+#define VIRTIO_TEST_PORT_ADD		1
+#define VIRTIO_TEST_PORT_REMOVE	2
+#define VIRTIO_TEST_PORT_READY	3
+#define VIRTIO_TEST_CONSOLE_PORT	4
+#define VIRTIO_TEST_RESIZE		5
+#define VIRTIO_TEST_PORT_OPEN	6
+#define VIRTIO_TEST_PORT_NAME	7
+
+struct virtio_test_control {
+	__virtio32 id;
+	__virtio16 event;
+	__virtio16 value;
 };
 
 #endif
