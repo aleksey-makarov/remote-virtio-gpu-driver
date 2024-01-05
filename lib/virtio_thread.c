e1715093078899bf0e613eb667e7d62aa4011178
Aleksei Makarov <aleksei.makarov@opensynergy.com>
2024-01-05 10:16:45 +0100

    gtk_gpu: Implement virtio_thread

--- lib/virtio_thread.c
+++ lib/virtio_thread.c
@@ -1,12 +1,41 @@
+#include <unistd.h>
+#include <stdbool.h>
+#include <pthread.h>
+#include <string.h>
+
 #include "error.h"
+
+static pthread_t thread;
+static volatile bool done = false;
+
+static void *virtio_thread(void *ptr)
+{
+	(void)ptr;
+
+	trace("enter");
+	while(!done) {
+		trace("");
+		sleep(3);
+	}
+	trace("exit");
+
+	return NULL;
+}
 
 int virtio_thread_start(void)
 {
 	trace("");
+	int err = pthread_create(&thread, NULL, virtio_thread, NULL);
+	if (err) {
+		error("pthread_create(): %s", strerror(err));
+		return -1;
+	}
 	return 0;
 }
 
 void virtio_thread_stop(void)
 {
 	trace("");
+	done = true;
+	pthread_join(thread, NULL);
 }
