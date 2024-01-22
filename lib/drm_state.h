0a9682f0b2ff7d7ac24b340fe63937485a3ecee5
Aleksei Makarov <aleksei.makarov@opensynergy.com>
2024-01-22 17:43:43 +0100

    test_virglrenderer: Wip: Compile drm_state.?

--- lib/drm_state.h
+++ lib/drm_state.h
@@ -1,10 +1,3 @@
-/*******************************************************************************
- * Copyright (c) 2023 OpenSynergy GmbH.
- *
- * This software may not be used in any way or distributed without
- * permission. All rights reserved.
- ******************************************************************************/
-
 #ifndef __drm_state_h__
 #define __drm_state_h__
 
@@ -15,7 +8,7 @@ void *drm_state_init(uint16_t *h, uint16_t *v);
 void drm_state_done(void);
 
 /* Returns native window */
-struct gbm_surface *drm_surface_create(uint32_t format);
+void *drm_surface_create(uint32_t format);
 void drm_surface_destroy(void);
 
 int drm_state_flip(void);
